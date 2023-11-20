0d858315fe0322c3a60e6de1c4ac326f1b9b3dca
Alfredo Augusto Petri <alfredo_petri@hotmail.com>
2023-11-20 12:03:54 -0300

    create conteudo-0.js e conteudo-1.js

---
+++ javascript/conteudo/Conteudo-0-Primeiro-arquivo-Javascript.js
@@ -0,0 +1,12 @@
+// A sintaxe usada para fazer um  comentário em javasvript é semelhante sintaxe de um comentário em css
+
+// comentário de uma linha
+
+/* isto é um comentário longo
+   de múltiplas linhas.
+ */
+
+// /* Você não pode, porém, /* aninhar comentários */ SyntaxError */
+
+console.log ("Hello, world!")
+// console.log () é a função usada para imprimir o que deve ser mostrado ao usuário (output) 
