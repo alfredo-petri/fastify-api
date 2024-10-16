46361123ae5620336a8c1dda8a70095f35eecabd
alfredo-petri <alfredo_petri@hotmail.com>
2024-10-16 08:31:26 -0300

    update

--- docker/kubernets/1_project/app.py
+++ docker/kubernets/1_project/app.py
@@ -10,4 +10,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(hot="0.0.0.0", debug=True, port="5000")
+    app.run(host="0.0.0.0", debug=True, port="5000")
+
+
