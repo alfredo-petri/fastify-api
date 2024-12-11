f0a2e0dfa88088df2b47342db1e5ad4326e868cc
alfredo-petri <alfredo_petri@hotmail.com>
2024-12-11 06:27:05 -0300

    rotas http

--- node/01-fundamentos-node/src/server.js
+++ node/01-fundamentos-node/src/server.js
@@ -1,6 +1,11 @@
 import http from "node:http"
 
 const server = http.createServer((req, res) => {
+    const { method, url } = req
+
+    if (method === "POST" && url === "/users") return res.end("criação de usuários")
+    if (method === "GET" && url === "/users") return res.end("busca de usuários")
+    
     return res.end("Hello World!!!")
 })
 
