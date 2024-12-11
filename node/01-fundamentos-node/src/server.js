6095cdfd4a3b7f6aac143c667fb9694498735ef0
alfredo-petri <alfredo_petri@hotmail.com>
2024-12-11 06:45:03 -0300

    salvando usuários em memória, stateful

--- node/01-fundamentos-node/src/server.js
+++ node/01-fundamentos-node/src/server.js
@@ -1,11 +1,20 @@
 import http from "node:http"
 
+const users = []
+
 const server = http.createServer((req, res) => {
     const { method, url } = req
 
-    if (method === "POST" && url === "/users") return res.end("criação de usuários")
-    if (method === "GET" && url === "/users") return res.end("busca de usuários")
-    
+    if (method === "POST" && url === "/users") {
+        users.push({
+            id: 1,
+            name: "Jhon Doe",
+            email: "jhondoe@example.com",
+        })
+        return res.end("criação de usuários")
+    }
+    if (method === "GET" && url === "/users") return res.setHeader("content-type", "aplication/json").end(JSON.stringify(users))
+
     return res.end("Hello World!!!")
 })
 
