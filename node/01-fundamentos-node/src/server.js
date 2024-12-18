5b042ea8d74a33043c4708efcdf6460775fa289a
alfredo-petri <alfredo_petri@hotmail.com>
2024-12-18 06:42:37 -0300

    midlewares

--- node/01-fundamentos-node/src/server.js
+++ node/01-fundamentos-node/src/server.js
@@ -1,22 +1,12 @@
 import http from "node:http"
+import { json } from "./midlewares/json.js"
 
 const users = []
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
 
-    const buffers = []
-
-    for await (const chunk of req) {
-        buffers.push(chunk)
-    }
-
-    try {
-        req.body = JSON.parse(Buffer.concat(buffers).toString())
-    } catch (error) {
-        req.body = null
-    }
-
+    await json(req, res)
     if (method === "POST" && url === "/users") {
         const { name, email } = req.body
 
@@ -27,7 +17,7 @@ const server = http.createServer(async (req, res) => {
         })
         return res.writeHead(201).end("criação de usuário bem sucedida")
     }
-    if (method === "GET" && url === "/users") return res.setHeader("content-type", "aplication/json").end(JSON.stringify(users))
+    if (method === "GET" && url === "/users") return res.end(JSON.stringify(users))
 
     return res.writeHead(404).end("Recurso não encontrado")
 })
