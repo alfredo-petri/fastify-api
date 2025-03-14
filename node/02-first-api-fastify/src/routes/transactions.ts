deeef7d9d7ec74cf8079179275a60ab5b4b72b25
alfredo-petri <alfredo_petri@hotmail.com>
2025-03-14 09:26:52 -0300

    create: transactionsRoutes plugin

---
+++ node/02-first-api-fastify/src/routes/transactions.ts
@@ -0,0 +1,5 @@
+import { FastifyInstance } from 'fastify'
+
+export async function transactionRoutes(app: FastifyInstance) {
+    console.log(app)
+}
--- node/02-first-api-fastify/src/server.ts
+++ node/02-first-api-fastify/src/server.ts
@@ -1,21 +1,11 @@
 import fastify from 'fastify'
-// import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
+import { transactionRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-    const transaction = await knex('transactions')
-        .where('amount', 1250)
-        .select('*')
-    // .insert({
-    //     id: crypto.randomUUID(),
-    //     title: 'transação de teste 2',
-    //     amount: 1250,
-    // })
-    // .returning('*')
-    return transaction
+app.register(transactionRoutes, {
+    prefix: 'transactions',
 })
 
 app.listen({
