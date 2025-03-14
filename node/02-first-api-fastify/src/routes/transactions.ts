780672383be618aa272f19c6e426333ac85c2229
alfredo-petri <alfredo_petri@hotmail.com>
2025-03-14 09:27:48 -0300

    feat: create transaction

--- node/02-first-api-fastify/src/routes/transactions.ts
+++ node/02-first-api-fastify/src/routes/transactions.ts
@@ -1,5 +1,25 @@
 import { FastifyInstance } from 'fastify'
+import { knex } from '../database'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
 
 export async function transactionRoutes(app: FastifyInstance) {
-    console.log(app)
+    app.post('/', async (request, reply) => {
+        const createTransactionBodySchema = z.object({
+            title: z.string(),
+            amount: z.number(),
+            type: z.enum(['debito', 'credito']),
+        })
+        const body = createTransactionBodySchema.parse(request.body)
+
+        const { amount, title, type } = body
+
+        await knex('transactions').insert({
+            id: randomUUID(),
+            title,
+            amount: type === 'credito' ? amount : -1 * amount,
+        })
+
+        return reply.status(201).send()
+    })
 }
