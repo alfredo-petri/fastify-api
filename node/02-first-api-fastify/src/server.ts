df477f59ab334e5f3f1c9f52f172cf1d07feae78
alfredo-petri <alfredo_petri@hotmail.com>
2025-03-14 07:17:07 -0300

    configure zod validations on env variables

--- node/02-first-api-fastify/src/server.ts
+++ node/02-first-api-fastify/src/server.ts
@@ -1,15 +1,25 @@
 import fastify from 'fastify'
+// import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-    const tables = await knex('sqlite_schema').select('*')
-    return tables
+    const transaction = await knex('transactions')
+        .where('amount', 1250)
+        .select('*')
+    // .insert({
+    //     id: crypto.randomUUID(),
+    //     title: 'transação de teste 2',
+    //     amount: 1250,
+    // })
+    // .returning('*')
+    return transaction
 })
 
 app.listen({
-    port: 3333,
+    port: env.PORT,
 }).then(() => {
-    console.log('HTTP Server Running!')
+    console.log('HTTP Server running at port ' + env.PORT)
 })
